8d32a136c872a84464c3858f973691aacbc0f5ee
androane <mihai.zamfir90@gmail.com>
2014-04-09 17:06:12 +0300

    updated themes view

--- gemet/thesaurus/views.py
+++ gemet/thesaurus/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from gemet.thesaurus.models import Namespace, Property
+from gemet.thesaurus.models import Namespace, Property, Language
 
 
 def themes_list(request):
-    ns = Namespace.objects.get(heading='Themes')
-    themes = Property.objects.filter(concept__namespace=ns, name='prefLabel')
-
-    return render(request, 'themes_list.html', {'themes': themes})
+    
+    if request.method=='GET':
+        #try to get the selected language. default is 'en'
+        try:
+            lang_code = request.GET['langcode']
+        except:
+            lang_code = 'en'
+        
+        #get all languages
+        langs = Language.objects.all()
+    
+        ns = Namespace.objects.get(heading='Themes')
+        themes = Property.objects.filter(concept__namespace=ns, name='prefLabel', language=lang_code)
+    
+        return render(request, 'themes_list.html', {'themes': themes,
+                                                    'langs': langs,
+                                                    'lang_code': lang_code})
