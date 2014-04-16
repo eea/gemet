0dead22edebcabc26815c4a8b0af86e4b16ee6aa
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-04-16 12:00:15 +0300

    urls: group urls with common prefix.

--- gemet/thesaurus/urls.py
+++ gemet/thesaurus/urls.py
@@ -1,28 +1,14 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from .views import themes_list, index, groups_list, concept, relations
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', index, name='index'),
-    url(
-        r'^(?P<langcode>[a-zA-Z-]+)/themes/$',
-        themes_list,
-        name='themes_list',
-    ),
-    url(
-        r'^(?P<langcode>[a-zA-Z-]+)/groups/$',
-        groups_list,
-        name='groups_list',
-    ),
-    url(
-        r'^(?P<langcode>[a-zA-Z-]+)/concept/(?P<concept_id>\d+)/$',
-        concept,
-        name='concept',
-    ),
-    url(
-        r'^(?P<langcode>[a-zA-Z-]+)/relations/(?P<group_id>\d+)/$',
-        relations,
-        name='relations',
-    ),
+    url(r'^(?P<langcode>[a-zA-Z-]+)/', include([
+        url(r'^themes/$', themes_list, name='themes_list'),
+        url(r'^groups/$', groups_list, name='groups_list'),
+        url(r'^concept/(?P<concept_id>\d+)/$', concept, name='concept'),
+        url(r'^relations/(?P<group_id>\d+)/$', relations, name='relations'),
+        ]))
 )
