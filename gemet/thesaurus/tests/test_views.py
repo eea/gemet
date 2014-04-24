90ce9b325acc0aaa195087156356297e90fa0d78
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-04-24 19:09:56 +0300

    Tests: test_one_supergroup_one_group

--- gemet/thesaurus/tests/test_views.py
+++ gemet/thesaurus/tests/test_views.py
@@ -84,12 +84,12 @@ class TestThemeConceptsView(WebTest):
     def setUp(self):
         LanguageFactory()
         NamespaceFactory()
+        ConceptFactory()
+        PropertyFactory()
 
     def test_one_theme_concept(self):
         NamespaceFactory(id=1, heading="Concepts")
 
-        ConceptFactory()
-        PropertyFactory()
         ConceptFactory(id=2, code="2", namespace_id=1)
         PropertyFactory(concept_id=2,
                         value="access to administrative documents"
@@ -103,7 +103,7 @@ class TestThemeConceptsView(WebTest):
 
         self.assertEqual(200, resp.status_int)
         self.assertEqual(resp.context['langcode'], 'en')
-        self.assertEqual(resp.pyquery('.themes li').length, 1)
+        self.assertEqual(resp.pyquery('.concepts li').length, 1)
 
         """
         after TO_DO list
@@ -120,9 +120,6 @@ class TestThemeConceptsView(WebTest):
     def test_more_theme_concepts(self):
         NamespaceFactory(id=1, heading="Concepts")
         PropertyTypeFactory(id=1)
-
-        ConceptFactory()
-        PropertyFactory()
 
         ConceptFactory(id=2, code="2", namespace_id=1)
         PropertyFactory(concept_id=2,
@@ -174,3 +171,48 @@ class TestThemeConceptsView(WebTest):
     def test_letter_selected(self):
         #TO DO
         pass
+
+
+class TestHierarchicalListings(WebTest):
+
+    def setUp(self):
+        NamespaceFactory(id=2, heading="Super groups")
+        ConceptFactory(namespace_id=2)
+        LanguageFactory()
+        PropertyFactory(value="SUPER_GROUP")
+
+    def test_one_supergroup_no_group(self):
+
+        url = reverse('groups', kwargs={'langcode': 'en'})
+        resp = self.app.get(url)
+
+        self.assertEqual(200, resp.status_int)
+        self.assertEqual(resp.context['langcode'], 'en')
+        self.assertEqual(resp.pyquery('.supergroups li').length, 1)
+        self.assertEqual(resp.pyquery('.groups li').length, 0)
+        self.assertEqual(resp.pyquery('.supergroups h2').text(), 'SUPER_GROUP')
+
+    def test_one_supergroup_one_group(self):
+
+        NamespaceFactory(id=3, heading="Groups")
+        ConceptFactory(id=2, code="2", namespace_id=3)
+        PropertyFactory(concept_id=2, value="GROUP")
+        PropertyTypeFactory(id=1, name="broader", label="broader term")
+        RelationFactory(property_type_id=1, source_id=2, target_id=1)
+        PropertyTypeFactory(id=2, name="narrower", label="narrower term")
+        RelationFactory(property_type_id=2, source_id=1, target_id=2)
+
+        url = reverse('groups', kwargs={'langcode': 'en'})
+        resp = self.app.get(url)
+
+        self.assertEqual(200, resp.status_int)
+        self.assertEqual(resp.context['langcode'], 'en')
+        self.assertEqual(resp.pyquery('.supergroups > li').length, 1)
+        self.assertEqual(resp.pyquery('.groups li').length, 1)
+        self.assertEqual(resp.pyquery('.supergroups h2').text(), 'SUPER_GROUP')
+        self.assertEqual(resp.pyquery('.groups li a').attr('href'),
+                         u'{url}'.format(url=reverse('relations',
+                                                     kwargs={'langcode': 'en',
+                                                             'group_id': 2}))
+                         )
+        self.assertEqual(resp.pyquery('.groups li a').text(), 'GROUP')
