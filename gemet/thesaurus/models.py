8d5245c40d73636ce0c7e513eadfbaec7f5ff7a5
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-04-25 18:38:58 +0300

    models: remove not null constraint from definition_source table fields.

--- gemet/thesaurus/models.py
+++ gemet/thesaurus/models.py
@@ -142,9 +142,9 @@ class ForeignRelation(Model):
 
 class DefinitionSource(Model):
     abbr = CharField(max_length=10, primary_key=True)
-    author = CharField(max_length=255)
-    title = CharField(max_length=255)
-    url = CharField(max_length=255)
-    publication = CharField(max_length=255)
-    place = CharField(max_length=255)
-    year = CharField(max_length=10)
+    author = CharField(max_length=255, null=True)
+    title = CharField(max_length=255, null=True)
+    url = CharField(max_length=255, null=True)
+    publication = CharField(max_length=255, null=True)
+    place = CharField(max_length=255, null=True)
+    year = CharField(max_length=10, null=True)
