a684f544e7ebd874d44ebe4323e7b0084b6903a4
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-04-30 17:15:14 +0300

    deployment: implement automated deployment, ref #19345.

---
+++ fabfile/__init__.py
@@ -0,0 +1,40 @@
+import ConfigParser
+from fabric.api import env
+from fabric.context_managers import cd, prefix
+from fabric.decorators import task
+from fabric.operations import require, run
+from path import path
+
+
+LOCAL_PATH = path(__file__).abspath().parent
+LOCATION_WARNING = """deployment. You need to prefix the task with the location
+    i.e.: fab staging deploy."""
+
+
+def environment(location):
+    config = ConfigParser.SafeConfigParser()
+    config.read(LOCAL_PATH / 'env.ini')
+    env.update(config.items(section=location))
+    env.sandbox_activate = 'source {0}'.format(path(env.sandbox) / 'bin' /
+                                               'activate')
+    env.deployment_location = location
+
+
+@task
+def staging():
+    environment('staging')
+
+
+@task
+def deploy():
+    require('deployment_location', used_for=LOCATION_WARNING)
+    require('project_root', provided_by=[environment])
+    require('fixture', provided_by=[environment])
+
+    with cd(env.project_root), prefix(env.sandbox_activate):
+        run('git pull --rebase')
+        run('pip install -r requirements-dep.txt')
+        run('./manage.py syncdb')
+        run('./manage.py loaddata {0}'.format(
+            path(env.project_root) / env.fixture))
+        run('supervisorctl -c {0} restart django'.format(env.supervisord_conf))
