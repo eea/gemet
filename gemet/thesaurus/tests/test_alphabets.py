ba635c0a89c0084e4e09a59ef0b212e7234bba1d
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-06-13 18:07:55 +0300

    tests: fix test for the alphates view.

--- gemet/thesaurus/tests/test_alphabets.py
+++ gemet/thesaurus/tests/test_alphabets.py
@@ -1,7 +1,6 @@
 from django_webtest import WebTest
 from django.core.urlresolvers import reverse
 
-from gemet.thesaurus.collation_charts import unicode_character_map
 from .factories import (
     LanguageFactory,
 )
@@ -15,16 +14,11 @@ class TestAlphabetsView(WebTest):
         url = reverse('alphabets', kwargs={'langcode': 'en'})
         resp = self.app.get(url)
 
-        letters = unicode_character_map.get('en', [])
-
         self.assertEqual(200, resp.status_int)
         self.assertEqual(resp.context['langcode'], 'en')
-        self.assertEqual(resp.pyquery('.content ul').children().size(), 26)
-        first_row = resp.pyquery('.letter:first').text().split()
-        self.assertEqual(first_row[0], '1')
-        self.assertEqual(first_row[1], 'a')
-        self.assertEqual(first_row[2], 'A')
-        last_row = resp.pyquery('.letter:last').text().split()
-        self.assertEqual(last_row[0], '26')
-        self.assertEqual(last_row[1], 'z')
-        self.assertEqual(last_row[2], 'Z')
+        self.assertEqual(resp.pyquery('.content ol').children().size(), 26)
+        letters = resp.pyquery('.letter').text().split()
+        self.assertEqual(letters[0], 'a')
+        self.assertEqual(letters[1], 'A')
+        self.assertEqual(letters[50], 'z')
+        self.assertEqual(letters[51], 'Z')
