6e32b0f0280252447a0d6a8a12d42a8ddee43186
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-06-16 14:52:53 +0300

    style: split concepts lists into two side-by-side divs.

--- gemet/thesaurus/templatetags/gemet_tags.py
+++ gemet/thesaurus/templatetags/gemet_tags.py
@@ -56,3 +56,16 @@ def get_default_name(concept_id):
         )
         .value
     )
+
+
+@register.filter(name="half")
+def half(elements, arg):
+    half_len = int(round(float(len(elements)) / 2))
+    half_list = []
+
+    if arg == 0:
+        half_list = elements[:half_len]
+    elif arg == 1:
+        half_list = elements[half_len:]
+
+    return half_list
