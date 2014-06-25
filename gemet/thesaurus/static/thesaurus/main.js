c54ffa90d3b643d31c7061b33e25e30efbc0c163
Valentin Popescu <valentin.popescu@eaudeweb.ro>
2014-06-25 12:58:55 +0300

    Fixed language select: forgot to remove console.log

--- gemet/thesaurus/static/thesaurus/main.js
+++ gemet/thesaurus/static/thesaurus/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-	console.log("REAdy");
 	// change language
 	// bind change event to select
   $('#js-change-language').on('change', function () {
     var url = $(this).find(':selected').attr('href'); // get selected value
-    console.log(url);
     if (url) { // require a URL
       window.location = url; // redirect
     }
