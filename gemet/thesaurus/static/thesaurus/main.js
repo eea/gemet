ba090f712a272626c9273199e629eb338244c1ef
Valentin Popescu <valentin.popescu@eaudeweb.ro>
2014-06-25 12:58:26 +0300

    Fixed language select

--- gemet/thesaurus/static/thesaurus/main.js
+++ gemet/thesaurus/static/thesaurus/main.js
@@ -1,7 +1,10 @@
-$(function(){
-  // bind change event to select
-  $('select#js-change-language').bind('change', function () {
+$(document).ready(function () {
+	console.log("REAdy");
+	// change language
+	// bind change event to select
+  $('#js-change-language').on('change', function () {
     var url = $(this).find(':selected').attr('href'); // get selected value
+    console.log(url);
     if (url) { // require a URL
       window.location = url; // redirect
     }
