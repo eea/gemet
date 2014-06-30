5c88c9e07c74850b87b14c61b26903cb874de389
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-06-30 13:41:55 +0300

    API: test fetch themes with factories.

---
+++ gemet/thesaurus/tests/api/test_fetch_themes.py
@@ -0,0 +1,42 @@
+from urllib import urlencode
+from xmlrpclib import Fault
+
+from django.core.urlresolvers import reverse
+
+from gemet.thesaurus.tests.factories import (
+    PropertyFactory,
+    ThemeFactory,
+    LanguageFactory,
+)
+from gemet.thesaurus.tests import GemetTest
+from gemet.thesaurus import DEFAULT_LANGCODE
+
+
+class TestFetchThemes(GemetTest):
+    def setUp(self):
+        self.english = LanguageFactory()
+        self.url = reverse('api_root') + 'fetchThemes?'
+        self.theme = ThemeFactory()
+        self._initialize(self.theme, 'prefLabel1', 'definition1', self.english)
+
+    def _initialize(self, concept, preflabel, definition, lang):
+        PropertyFactory(
+            concept=concept, name='prefLabel', value=preflabel, language=lang
+        )
+        PropertyFactory(
+            concept=concept, name='definition', value=definition, language=lang
+        )
+
+    def _response_valid(self, status, content_type):
+        self.assertEqual(200, status)
+        self.assertEqual(content_type, 'application/json')
+
+    def test_default_language_parameter(self):
+        resp = self.app.get(self.url)
+        self._response_valid(resp.status_int, resp.content_type)
+        self.assertEqual(resp.json[0]['preferredLabel']['language'],
+                         DEFAULT_LANGCODE)
+
+    def test_invalid_language(self):
+        self.assertRaises(Fault, self.app.get,
+                          self.url + urlencode({'language': 'es'}))
