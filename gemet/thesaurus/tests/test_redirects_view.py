25bb67b42fd70ae4f1ec55e6890cad8bd8ea4d70
Vitalie Maldur <maldur.vitalik@gmail.com>
2014-07-03 15:40:27 +0300

    tests: test_redirects_view improvement

--- gemet/thesaurus/tests/test_redirects_view.py
+++ gemet/thesaurus/tests/test_redirects_view.py
@@ -1,5 +1,6 @@
 from .factories import (
     LanguageFactory,
+    ThemeFactory,
 )
 from . import GemetTest
 
@@ -16,4 +17,21 @@ class TestRedirectsView(GemetTest):
     def test_groups(self):
         resp = self.app.get('/groups')
 
+        self.assertEqual(301, resp.status_int)
+
+    def test_index_html(self):
+        theme = ThemeFactory()
+        resp = self.app.get(u'/theme_concepts?th={th}&langcode={langcode}'
+                            .format(
+                                th = theme.id,
+                                langcode='en-US',
+                            ))
+        self.assertEqual(301, resp.status_int)
+
+    def test_alphabetic(self):
+        resp = self.app.get(u'/alphabetic?letter={letter}&langcode={langcode}'
+                            .format(
+                                letter=1,
+                                langcode='en-US',
+                            ))
         self.assertEqual(301, resp.status_int)
