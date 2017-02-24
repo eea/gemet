b1b798516f42f7a958c38b8286c631ab7b71fa50
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2017-02-24 15:29:48 +0200

    [refs #82698] Move style definition to template

--- gemet/layout.py
+++ gemet/layout.py
@@ -14,12 +14,15 @@ def layout_context_processor(request):
         header_text = prepare_html(resp.text)
         title_start = header_text.find('<title>')
         title_end = header_text.find('</title>')
-        HEADER_TEXT_PREFFIX = header_text[:title_start + len('<title>')]
-        HEADER_TEXT_SUFFIX = header_text[title_end:]
+        head_end = header_text.find('</head>')
+        HEADER_BEFORE_TITLE = header_text[:title_start + len('<title>')]
+        HEADER_AFTER_TITLE = header_text[title_end:head_end]
+        HEADER_END = header_text[head_end:]
         resp = requests.get(FOOTER_URL, headers={'Authorization': auth_header})
         FOOTER_TEXT = prepare_html(resp.text)
-        return {'header_text_preffix': HEADER_TEXT_PREFFIX,
-                'header_text_suffix': HEADER_TEXT_SUFFIX,
+        return {'header_before_title': HEADER_BEFORE_TITLE,
+                'header_after_title': HEADER_AFTER_TITLE,
+                'header_end': HEADER_END,
                 'footer_text': FOOTER_TEXT,
                 'layout_template': 'layout_zope.html'}
     else:
@@ -29,8 +32,6 @@ def layout_context_processor(request):
 def prepare_html(html):
     html = html.replace('href="/', 'href="' + ZOPE_URL + '/')
     html = html.replace('src="/', 'src="' + ZOPE_URL + '/')
-    css_url = '<link rel="stylesheet" href="' + static('thesaurus/style.css') \
-              + '" />'
     js_url = '<script src="' + static('thesaurus/main.js') + '"></script>'
     jquery_url = '<script src=' + \
         '"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"' + \
@@ -41,6 +42,5 @@ def prepare_html(html):
     breadcrumbs_old = '<div class="breadcrumbitemlast eionetaccronym">' + \
         'Eionet</div>'
     html = html.replace(breadcrumbs_old, breadcrumbs)
-    html = html.replace('</head>', css_url + '</head>')
     html = html.replace('</body>', jquery_url + js_url + '</body>')
     return html
