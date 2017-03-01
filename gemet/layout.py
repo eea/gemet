9818af115ee9942da7d87cad4e3edf6e8e098da8
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2017-03-01 17:43:03 +0200

    [refs #82814] Use local static files

--- gemet/layout.py
+++ gemet/layout.py
@@ -1,4 +1,3 @@
-from django.templatetags.static import static
 import requests
 from django.conf import settings
 
@@ -32,15 +31,10 @@ def layout_context_processor(request):
 def prepare_html(html):
     html = html.replace('href="/', 'href="' + ZOPE_URL + '/')
     html = html.replace('src="/', 'src="' + ZOPE_URL + '/')
-    js_url = '<script src="' + static('thesaurus/main.js') + '"></script>'
-    jquery_url = '<script src=' + \
-        '"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"' + \
-        '></script>'
     breadcrumbs = '<div class="breadcrumbitem eionetaccronym">' + \
         '<a href="{zope_url}">Eionet</a></div>'.format(zope_url=ZOPE_URL) + \
         '<div class="breadcrumbitemlast">GEMET</div>'
     breadcrumbs_old = '<div class="breadcrumbitemlast eionetaccronym">' + \
         'Eionet</div>'
     html = html.replace(breadcrumbs_old, breadcrumbs)
-    html = html.replace('</body>', jquery_url + js_url + '</body>')
     return html
