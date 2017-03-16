1bbff3d25949f4821a5e5dc40d9a06c2c7c5bf1e
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-03-16 16:05:20 +0200

    [refs #81278] Add version form with identifier field

--- gemet/thesaurus/forms.py
+++ gemet/thesaurus/forms.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.auth.forms import AuthenticationForm
 
 from gemet.thesaurus.models import ForeignRelation, Language, Namespace
-from gemet.thesaurus.models import Property
+from gemet.thesaurus.models import Property, Version
 
 
 class SearchForm(forms.Form):
@@ -54,3 +54,26 @@ class LDAPAuthenticationForm(AuthenticationForm):
         if user.username not in settings.AUTHORIZED_USERS:
             raise forms.ValidationError(
                 'Your account is not authorized to login to GEMET')
+
+
+def my_choices():
+    current_identifier = Version.objects.get(is_current=True).identifier
+    version_numbers = map(int, current_identifier.split("."))
+    choices = []
+    version_numbers[2] += 1
+    identifier = ".".join(map(str, version_numbers))
+    choices.append((identifier, identifier))
+    version_numbers[2] = 0
+    version_numbers[1] += 1
+    identifier = ".".join(map(str, version_numbers))
+    choices.append((identifier, identifier))
+    version_numbers[1] = 0
+    version_numbers[0] += 1
+    identifier = ".".join(map(str, version_numbers))
+    choices.append((identifier, identifier))
+    return choices
+
+
+class VersionForm(forms.Form):
+
+    version = forms.ChoiceField(choices=my_choices)
