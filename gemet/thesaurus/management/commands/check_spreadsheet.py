91df603fe9e224153639c91c8595614f051fa2dc
Andrei Melis <andrei.melis@eaudeweb.ro>
2017-03-23 14:59:43 +0200

    refs #80373 Cosmetic changes in check_spreadsheet command

--- gemet/thesaurus/management/commands/check_spreadsheet.py
+++ gemet/thesaurus/management/commands/check_spreadsheet.py
@@ -34,19 +34,19 @@ class Command(BaseCommand):
 
     def check_term_existence(self, term, term_type, excel_cell_value,
                              new_terms):
-        message = 'Error at cell {}: "{}"'.format(excel_cell_value, term)
+        message = ' at cell {}: "{}"'.format(excel_cell_value, term)
         term_in_database = Property.objects.filter(value=term).exists()
         if term_type:
             if not term_in_database:
-                message += ' not defined in database.'
-                message += ' Term found in column "Label".' \
-                    if term in new_terms else ''
+                message += ' not found'
+                message += ' in database, but found in spreadsheet. [WARNING]' \
+                    if term in new_terms else '. [ERROR]'
                 self.stdout.write(message)
         else:
             if term not in new_terms:
-                message += ' not found in column "Label".'
-                message += ' Term found in database.' \
-                    if term_in_database else ''
+                message += ' not found'
+                message += ' in spreadsheet, but found in database. [WARNING]' \
+                    if term_in_database else '. [ERROR]'
                 self.stdout.write(message)
 
     def handle(self, *args, **options):
@@ -59,7 +59,8 @@ class Command(BaseCommand):
 
         sheet = wb.active
         new_terms = [x.value.strip().lower() for x, in
-                     sheet.iter_rows(min_col=1, max_col=1, min_row=2)]
+                     sheet.iter_rows(min_col=1, max_col=1, min_row=2)
+                     if x.value is not None]
 
         for label_cell, in sheet.iter_cols(min_row=1, max_row=1, min_col=1):
             if label_cell.value not in LABELS:
@@ -67,7 +68,7 @@ class Command(BaseCommand):
 
             term_type = LABELS.get(label_cell.value)
             for cell, in sheet.iter_rows(min_col=label_cell.col_idx,
-                                        max_col=label_cell.col_idx, min_row=2):
+                                         max_col=label_cell.col_idx, min_row=2):
                 if not cell.value:
                     continue
                 correct_terms = split_text_into_terms(cell.value)
