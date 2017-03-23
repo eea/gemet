ca8280613fbb19db3e0244e9a215ad2b44361dfc
mihai-macaneata <themacaneata@gmail.com>
2017-03-23 19:28:11 +0200

    repositioning of the menu so it doesn't look like the search is related to the menu anymore

--- gemet/thesaurus/static/thesaurus/js/main.js
+++ gemet/thesaurus/static/thesaurus/js/main.js
@@ -1,49 +1,95 @@
+$(document).ready(function() {
+    // change language
+    // bind change event to select
 
-$(document).ready(function () {
-  // change language
-  // bind change event to select
+    $('#js-change-language').on('change', function() {
+        var url = $(this).find(':selected').attr('href'); // get selected value
+        if (url) { // require a URL
+            window.location = url; // redirect
+        }
+        return false;
+    });
 
-  $('#js-change-language').on('change', function () {
-    var url = $(this).find(':selected').attr('href'); // get selected value
-    if (url) { // require a URL
-      window.location = url; // redirect
+
+
+
+    // $('.search_bar').on('mouseenter', function() {
+
+    //     if ((window.location.href.indexOf("search") == -1)&&!($('.search-link').hasClass('active'))) {
+    //         $(this).css('border-top-right-radius', '0')
+    //             setTimeout(function(){ 
+    //         $('.active').addClass('remove-active');
+    //         $('.search-link').addClass('active');
+
+    //          }, 200);
+    //     }
+
+
+    // });
+
+    // $('.search_bar').on('mouseleave', function() {
+    //     if (window.location.href.indexOf("search") == -1) {
+    // $(this).css('border-top-right-radius', '6px')
+    //       setTimeout(function(){ 
+    //         $('.search-link').removeClass('active');
+    //         $('.active').removeClass('remove-active');
+    //       }, 200);
+        
+
+    //     }
+    // });
+
+
+    $('.search-link').on('click',function(){
+      $('.search_bar input#id_query').trigger('focus');
+      // if (window.location.href.indexOf("search") == -1) {
+      //                 $('.search_bar').css('border-top-right-radius', '0')
+      //     setTimeout(function(){ 
+
+     
+      //              $('.active').addClass('remove-active');
+      //       $('.search-link').addClass('active');
+      //     }, 200);
+      //   }
+
+    })
+
+
+
+
+    $('.relations ul').filter(function() {
+        return $(this).text().trim().length == 0 }).remove();
+
+    if (window.matchMedia("(max-width: 800px)").matches) {
+        $('.expand-button').on('click', function() {
+            $(this).parent().toggleClass('expand');
+            $('body').toggleClass('sidebar-open');
+        });
+
+        $('.backdrop-site').on('click', function() {
+            $('.gemet-menu').removeClass('expand');
+            $('.side-bar').removeClass('expand');
+            $('body').removeClass('sidebar-open');
+        })
+
     }
-    return false;
-  });
-
-
-$('.relations ul').filter(function(){return $(this).text().trim().length==0}).remove();
-
-if (window.matchMedia("(max-width: 800px)").matches) {
- $('.expand-button').on('click', function(){
-  $(this).parent().toggleClass('expand');
-  $('body').toggleClass('sidebar-open');
-});
-
-$('.backdrop-site').on('click',function(){
-    $('.gemet-menu').removeClass('expand');
-    $('.side-bar').removeClass('expand');
-  $('body').removeClass('sidebar-open');
-})
-
-}
 
 
 
-if (window.matchMedia("(min-width: 800px)").matches) {
-$first_menu_item = $('.gemet-menu a:first-of-type');
-$last_menu_item = $('.gemet-menu a:last-of-type');
+    // if (window.matchMedia("(min-width: 800px)").matches) {
+    //     $first_menu_item = $('.gemet-menu a:first-of-type');
+    //     $last_menu_item = $('.gemet-menu a:last-of-type');
 
-if($first_menu_item.hasClass('active')) {
-  $('.search_bar').css ('border-top-left-radius', '0');
-  console.log('first');
-}
+    //     if ($first_menu_item.hasClass('active')) {
+    //         $('.search_bar').css('border-top-left-radius', '0');
+    //         console.log('first');
+    //     }
 
-if($last_menu_item.hasClass('active')) {
-   $('.search_bar').css ('border-top-right-radius', '0');
-   console.log('last');
-}
-}
+    //     if ($last_menu_item.hasClass('active')) {
+    //         $('.search_bar').css('border-top-right-radius', '0');
+    //         console.log('last');
+    //     }
+    // }
 });
 
 /**
@@ -56,55 +102,54 @@ if($last_menu_item.hasClass('active')) {
  *
  */
 $.fn.extend({
-  treeview: function() {
-    return this.each(function() {
-      // Initialize the top levels;
-      var tree = $(this);
+    treeview: function() {
+        return this.each(function() {
+            // Initialize the top levels;
+            var tree = $(this);
 
-      tree.addClass('treeview-tree');
-      tree.find('li').each(function() {
-        var stick = $(this);
-      });
-      tree.find('li').has("ul").each(function () {
-        var branch = $(this); //li with children ul
+            tree.addClass('treeview-tree');
+            tree.find('li').each(function() {
+                var stick = $(this);
+            });
+            tree.find('li').has("ul").each(function() {
+                var branch = $(this); //li with children ul
 
-        branch.prepend("<i class='tree-indicator glyphicon glyphicon-chevron-right'></i>");
-        branch.addClass('tree-branch');
-        branch.on('click', function (e) {
-          if (this == e.target) {
-            var icon = $(this).children('i:first');
+                branch.prepend("<i class='tree-indicator glyphicon glyphicon-chevron-right'></i>");
+                branch.addClass('tree-branch');
+                branch.on('click', function(e) {
+                    if (this == e.target) {
+                        var icon = $(this).children('i:first');
 
-            icon.toggleClass("glyphicon-chevron-down glyphicon-chevron-right");
-            $(this).children().children().toggle();
-          }
-        })
-        branch.children().children().toggle();
+                        icon.toggleClass("glyphicon-chevron-down glyphicon-chevron-right");
+                        $(this).children().children().toggle();
+                    }
+                })
+                branch.children().children().toggle();
 
-        /**
-         *  The following snippet of code enables the treeview to
-         *  function when a button, indicator or anchor is clicked.
-         *
-         *  It also prevents the default function of an anchor and
-         *  a button from firing.
-         */
-        branch.children('.tree-indicator, button, a').click(function(e) {
-          branch.click();
+                /**
+                 *  The following snippet of code enables the treeview to
+                 *  function when a button, indicator or anchor is clicked.
+                 *
+                 *  It also prevents the default function of an anchor and
+                 *  a button from firing.
+                 */
+                branch.children('.tree-indicator, button, a').click(function(e) {
+                    branch.click();
 
-          e.preventDefault();
+                    e.preventDefault();
+                });
+            });
         });
-      });
-    });
-  }
+    }
 });
 
 /**
  *  The following snippet of code automatically converst
  *  any '.treeview' DOM elements into a treeview component.
  */
-$(window).on('load', function () {
-  $('.treeview').each(function () {
-    var tree = $(this);
-    tree.treeview();
-  })
+$(window).on('load', function() {
+    $('.treeview').each(function() {
+        var tree = $(this);
+        tree.treeview();
+    })
 })
-
