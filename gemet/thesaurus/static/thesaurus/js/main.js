0bae82c67cecfb462aa4b46fc6fa7301e843dac8
mihai-macaneata <themacaneata@gmail.com>
2017-03-28 17:48:00 +0300

    [refs #79717] lists now break-word when needed

--- gemet/thesaurus/static/thesaurus/js/main.js
+++ gemet/thesaurus/static/thesaurus/js/main.js
@@ -1,95 +1,84 @@
+$(document).ready(function() {
+    // change language
+    // bind change event to select
 
-$(document).ready(function () {
-  // change language
-  // bind change event to select
-
-        if (window.location.href.indexOf("search") > -1) { 
-          $('.search-button').css('display','inline-block');
-         console.log('sfasfsa');
-        }
-
-         if($('.alphabet').hasClass('active')){
-              $('.search_bar').css ('border-top-right-radius', '0');
-          }
-
-
-  $('#js-change-language').on('change', function () {
-    var url = $(this).find(':selected').attr('href'); // get selected value
-    if (url) { // require a URL
-      window.location = url; // redirect
+    if (window.location.href.indexOf("search") > -1) {
+        $('.search-button').css('display', 'inline-block');
+        console.log('sfasfsa');
     }
-    return false;
-  });
 
-  $('.source.input-area').on('click', function(){
-    var $textArea = $(this).siblings('.source.text-area').first();
-    var $button = $(this)
-    if ($button.text() === "Show source" &&
-    !($textArea.hasClass('contains-sources'))){
-      var url = $(this).data('href');
-      $.ajax({
-        type: "GET",
-        url: url,
-        success: function(data){
-          $textArea.html(data);
+
+    $('#js-change-language').on('change', function() {
+        var url = $(this).find(':selected').attr('href'); // get selected value
+        if (url) { // require a URL
+            window.location = url; // redirect
         }
-      })
-    }
-    $textArea.toggle();
-    $textArea.addClass('contains-sources');
-    $button.text($button.text() === "Show source" ? "Hide source" : "Show source");
-  });
+        return false;
+    });
 
-  $('.history-details.input-area').on('click', function(){
-    var $textArea = $(this).siblings('.history-details.text-area').first();
-    var $button = $(this)
-    if ($button.text() === "See details" &&
-    !($textArea.hasClass('contains-details'))){
-      var url = $(this).data('href');
-      $.ajax({
-        type: "GET",
-        url: url,
-        success: function(data){
-          $textArea.html(data);
+    $('.source.input-area').on('click', function() {
+        var $textArea = $(this).siblings('.source.text-area').first();
+        var $button = $(this)
+        if ($button.text() === "Show source" &&
+            !($textArea.hasClass('contains-sources'))) {
+            var url = $(this).data('href');
+            $.ajax({
+                type: "GET",
+                url: url,
+                success: function(data) {
+                    $textArea.html(data);
+                }
+            })
         }
-      })
+        $textArea.toggle();
+        $textArea.addClass('contains-sources');
+        $button.text($button.text() === "Show source" ? "Hide source" : "Show source");
+    });
+
+    $('.history-details.input-area').on('click', function() {
+        var $textArea = $(this).siblings('.history-details.text-area').first();
+        var $button = $(this)
+        if ($button.text() === "See details" &&
+            !($textArea.hasClass('contains-details'))) {
+            var url = $(this).data('href');
+            $.ajax({
+                type: "GET",
+                url: url,
+                success: function(data) {
+                    $textArea.html(data);
+                }
+            })
+        }
+        $textArea.toggle();
+        $textArea.addClass('contains-details');
+        $button.text($button.text() === "See details" ? "Hide details" : "See details");
+    });
+    $('.relations ul').filter(function() {
+        return $(this).text().trim().length == 0 }).remove();
+
+    if (window.matchMedia("(max-width: 800px)").matches) {
+        $('.expand-button').on('click', function() {
+            $(this).parent().toggleClass('expand');
+            $('body').toggleClass('sidebar-open');
+        });
+
+        $('.backdrop-site').on('click', function() {
+            $('.gemet-menu').removeClass('expand');
+            $('.side-bar').removeClass('expand');
+            $('body').removeClass('sidebar-open');
+        })
+
     }
-    $textArea.toggle();
-    $textArea.addClass('contains-details');
-    $button.text($button.text() === "See details" ? "Hide details" : "See details");
-  });
-$('.relations ul').filter(function(){return $(this).text().trim().length==0}).remove();
-
-if (window.matchMedia("(max-width: 800px)").matches) {
- $('.expand-button').on('click', function(){
-  $(this).parent().toggleClass('expand');
-  $('body').toggleClass('sidebar-open');
-});
-
-$('.backdrop-site').on('click',function(){
-    $('.gemet-menu').removeClass('expand');
-    $('.side-bar').removeClass('expand');
-  $('body').removeClass('sidebar-open');
-})
-
-}
 
 
 
-if (window.matchMedia("(min-width: 800px)").matches) {
-$first_menu_item = $('.gemet-menu a:first-of-type');
-$last_menu_item = $('.gemet-menu a:last-of-type');
+    if (window.matchMedia("(min-width: 800px)").matches) {
+        $last_menu_item = $('.search-button');
+        if ($last_menu_item.hasClass('active')) {
+            $('.search_bar').css('border-top-right-radius', '0');
 
-if($first_menu_item.hasClass('active')) {
-  $('.search_bar').css ('border-top-left-radius', '0');
-  
-}
-
-if($last_menu_item.hasClass('active')) {
-   $('.search_bar').css ('border-top-right-radius', '0');
-   
-}
-}
+        }
+    }
 });
 
 /**
@@ -102,55 +91,54 @@ if($last_menu_item.hasClass('active')) {
  *
  */
 $.fn.extend({
-  treeview: function() {
-    return this.each(function() {
-      // Initialize the top levels;
-      var tree = $(this);
+    treeview: function() {
+        return this.each(function() {
+            // Initialize the top levels;
+            var tree = $(this);
 
-      tree.addClass('treeview-tree');
-      tree.find('li').each(function() {
-        var stick = $(this);
-      });
-      tree.find('li').has("ul").each(function () {
-        var branch = $(this); //li with children ul
+            tree.addClass('treeview-tree');
+            tree.find('li').each(function() {
+                var stick = $(this);
+            });
+            tree.find('li').has("ul").each(function() {
+                var branch = $(this); //li with children ul
 
-        branch.prepend("<i class='tree-indicator glyphicon glyphicon-chevron-right'></i>");
-        branch.addClass('tree-branch');
-        branch.on('click', function (e) {
-          if (this == e.target) {
-            var icon = $(this).children('i:first');
+                branch.prepend("<i class='tree-indicator glyphicon glyphicon-chevron-right'></i>");
+                branch.addClass('tree-branch');
+                branch.on('click', function(e) {
+                    if (this == e.target) {
+                        var icon = $(this).children('i:first');
 
-            icon.toggleClass("glyphicon-chevron-down glyphicon-chevron-right");
-            $(this).children().children().toggle();
-          }
-        })
-        branch.children().children().toggle();
+                        icon.toggleClass("glyphicon-chevron-down glyphicon-chevron-right");
+                        $(this).children().children().toggle();
+                    }
+                })
+                branch.children().children().toggle();
 
-        /**
-         *  The following snippet of code enables the treeview to
-         *  function when a button, indicator or anchor is clicked.
-         *
-         *  It also prevents the default function of an anchor and
-         *  a button from firing.
-         */
-        branch.children('.tree-indicator, button, a').click(function(e) {
-          branch.click();
+                /**
+                 *  The following snippet of code enables the treeview to
+                 *  function when a button, indicator or anchor is clicked.
+                 *
+                 *  It also prevents the default function of an anchor and
+                 *  a button from firing.
+                 */
+                branch.children('.tree-indicator, button, a').click(function(e) {
+                    branch.click();
 
-          e.preventDefault();
+                    e.preventDefault();
+                });
+            });
         });
-      });
-    });
-  }
+    }
 });
 
 /**
  *  The following snippet of code automatically converst
  *  any '.treeview' DOM elements into a treeview component.
  */
-$(window).on('load', function () {
-  $('.treeview').each(function () {
-    var tree = $(this);
-    tree.treeview();
-  })
+$(window).on('load', function() {
+    $('.treeview').each(function() {
+        var tree = $(this);
+        tree.treeview();
+    })
 })
-
