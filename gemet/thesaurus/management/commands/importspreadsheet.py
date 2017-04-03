c859dbe8f01c78e43ce64bba2c748030375721ba
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2017-04-03 14:44:54 +0300

    [refs #80373] Case insensitive concept match in import command

--- gemet/thesaurus/management/commands/importspreadsheet.py
+++ gemet/thesaurus/management/commands/importspreadsheet.py
@@ -6,6 +6,7 @@ from django.core.management.base import BaseCommand
 
 from gemet.thesaurus import PENDING, PUBLISHED
 from gemet.thesaurus import models
+from gemet.thesaurus.management.utils import get_search_text
 from gemet.thesaurus.utils import get_new_code, split_text_into_terms
 from gemet.thesaurus.utils import has_reverse_relation, create_reverse_relation
 
@@ -31,35 +32,47 @@ class Command(BaseCommand):
         self.language = models.Language.objects.get(code=LANGCODE)
         self.version = models.Version.under_work()
         self.namespace = models.Namespace.objects.get(heading=NAMESPACE)
+        self.concepts = {}
+        self.properties = []
 
         self.stdout.write('Creating concepts...')
         self._create_concepts(sheet)
         self.stdout.write('Creating relations...')
         self._create_relations(sheet)
 
+        self.stdout.write('Creating {} properties...'
+                          .format(len(self.properties)))
+        models.Property.objects.bulk_create(self.properties, batch_size=10000)
+
     def _create_concepts(self, sheet):
         for row in sheet.iter_rows(max_col=3, min_row=2):
-            label, definition, source = [(cell.value or '').strip()
-                                         for cell in row]
+            label, defin, source = [(cell.value or '').strip() for cell in row]
 
             if not label:
                 continue
 
+            properties = {
+                'prefLabel': label,
+                'definition': defin,
+                'source': source,
+            }
+            is_new_concept = False
+
             property = models.Property.objects.filter(
                 name='prefLabel',
-                value=label,
+                value__iexact=label,
                 language=self.language,
             ).first()
 
-            skip_pref = False
-
             if property:
                 concept = property.concept
-                self.stdout.write(u'Concept {} exists'.format(label))
+                msg = u'Concept {} exists. '.format(label)
                 if property.status in [PENDING, PUBLISHED]:
-                    skip_pref = True
-                    self.stdout.write('Skipping prefLabel creation')
+                    del properties['prefLabel']
+                    msg += 'Skipping prefLabel creation.'
+                self.stdout.write(msg)
             else:
+                is_new_concept = True
                 code = get_new_code(self.namespace)
 
                 concept = models.Concept.objects.create(
@@ -70,23 +83,29 @@ class Command(BaseCommand):
                 )
                 self.stdout.write(u'Concept added: {}'.format(label))
 
-            properties = {
-                'prefLabel': label,
-                'definition': definition,
-                'source': source,
-            }
+            self.concepts[label.lower()] = concept
             for name, value in properties.iteritems():
-                if skip_pref and name == 'prefLabel':
-                    continue
-                models.Property.objects.create(
-                    status=PENDING,
-                    version_added=self.version,
+                if not models.Property.objects.filter(
                     concept=concept,
                     language=self.language,
                     name=name,
                     value=value,
-                )
-            self.stdout.write(u'Properties added for: {}'.format(label))
+                    status__in=[PUBLISHED, PENDING],
+                ).exists():
+                    is_new_concept = True
+                    self.properties.append(models.Property(
+                        status=PENDING,
+                        version_added=self.version,
+                        concept=concept,
+                        language=self.language,
+                        name=name,
+                        value=value,
+                    ))
+            if is_new_concept:
+                search_text = get_search_text(
+                    concept.id, self.language.code, PENDING, self.version)
+                if search_text:
+                    self.properties.append(search_text)
 
     def _create_relations(self, sheet):
         def get_terms(row, idx1, idx2):
@@ -109,26 +128,12 @@ class Command(BaseCommand):
                 narrower: get_terms(row, 7, 8),
             }
 
-            source = (
-                models.Property.objects
-                .get(name='prefLabel', value=label, language=self.language)
-                .concept
-            )
+            source = self.concepts[label.lower()]
 
             for property_type, terms in relations.iteritems():
                 for term in terms:
-                    try:
-                        target = (
-                            models.Property.objects
-                            .get(
-                                name='prefLabel',
-                                value=term,
-                                language=self.language,
-                                concept__namespace=self.namespace,
-                            )
-                            .concept
-                        )
-                    except models.Property.DoesNotExist:
+                    target = self._get_concept(term)
+                    if not target:
                         code = get_new_code(self.namespace)
                         target = models.Concept.objects.create(
                             code=code,
@@ -136,14 +141,14 @@ class Command(BaseCommand):
                             version_added=self.version,
                             status=PENDING,
                         )
-                        models.Property.objects.create(
+                        self.properties.append(models.Property(
                             status=PENDING,
                             version_added=self.version,
                             concept=target,
                             language=self.language,
                             name='prefLabel',
                             value=term,
-                        )
+                        ))
                         self.stdout.write('Inexistent concept: {}'.format(term))
 
                     relation = models.Relation.objects.filter(
@@ -166,3 +171,23 @@ class Command(BaseCommand):
                     reverse_relation = create_reverse_relation(relation)
                     self.stdout.write('Reverse relation created: {}'
                                       .format(reverse_relation))
+
+    def _get_concept(self, label):
+        concept = self.concepts.get(label.lower())
+
+        if not concept:
+            try:
+                concept = (
+                    models.Property.objects
+                    .get(
+                        name='prefLabel',
+                        value__iexact=label,
+                        language=self.language,
+                        concept__namespace=self.namespace,
+                    )
+                    .concept
+                )
+            except models.Property.DoesNotExist:
+                concept = None
+
+        return concept
