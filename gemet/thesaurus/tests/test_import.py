1ebd93c9fe4f1e4eab96f6d3752f8dade53a258a
Ariel Pontes <pontes.ariel@gmail.com>
2021-01-07 17:41:25 +0200

    Add support for Themes

--- gemet/thesaurus/tests/test_import.py
+++ gemet/thesaurus/tests/test_import.py
@@ -1,8 +1,8 @@
 from django.core.files import File
 from django.test import TestCase, Client
 
-from .factories import VersionFactory
-from gemet.thesaurus.models import Concept, Import, Property
+from .factories import VersionFactory, ConceptFactory
+from gemet.thesaurus.models import Concept, Import, Property, Namespace
 
 
 class ConceptImportView(TestCase):
@@ -11,28 +11,52 @@ class ConceptImportView(TestCase):
     fixtures = ['data.json']
 
     def setUp(self):
-        VersionFactory(identifier='')
+        version = VersionFactory(identifier='')
         self.client = Client()
 
+        concept_namespace = Namespace.objects.get(heading='Concepts')
+        group_namespace = Namespace.objects.get(heading='Groups')
+        theme_namespace = Namespace.objects.get(heading='Themes')
+
+        i = 1
+        # Create broader concepts
+        for value in ['pollution', 'impact source']:
+            concept = ConceptFactory(namespace=concept_namespace, code=str(i))
+            concept.properties.create(
+                name='prefLabel', value=value, language_id='en',
+                version_added=version
+            )
+            i += 1
+
+        # Create groups
+        for value in ['BIOSPHERE', 'HYDROSPHERE']:
+            concept = ConceptFactory(namespace=group_namespace, code=str(i))
+            concept.properties.create(
+                name='prefLabel', value=value, language_id='en',
+                version_added=version
+            )
+            i += 1
+
+        # Create themes
+        for value in ['Fake Theme']:
+            concept = ConceptFactory(namespace=theme_namespace, code=str(i))
+            concept.properties.create(
+                name='prefLabel', value=value, language_id='en',
+                version_added=version
+            )
+            i += 1
+
     def test_import_concepts_and_translations_together(self):
+        num_concepts_before = Concept.objects.count()
         import_obj = Import.objects.create(
             spreadsheet=File(open('gemet/thesaurus/tests/files/concepts.xlsx'))
         )
         url = '/import/{}/start/'.format(import_obj.pk)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            Concept.objects.filter(
-                status=0, namespace__heading='Concepts'
-            ).count(),
-            79
-        )
-        self.assertEqual(
-            Concept.objects.filter(
-                status=0, namespace__heading='Groups'
-            ).count(),
-            2
-        )
+        # 45 new concepts were imported
+        num_concepts_after = Concept.objects.count()
+        self.assertEqual(num_concepts_after - num_concepts_before, 45)
         # Notes are also imported
         self.assertEqual(
             Property.objects.filter(
@@ -42,6 +66,7 @@ class ConceptImportView(TestCase):
         )
 
     def test_import_concepts_and_translations_separately(self):
+        num_concepts_before = Concept.objects.count()
         # Import concepts in English first
         import_obj = Import.objects.create(
             spreadsheet=File(open('gemet/thesaurus/tests/files/only_en.xlsx'))
@@ -49,18 +74,10 @@ class ConceptImportView(TestCase):
         url = '/import/{}/start/'.format(import_obj.pk)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            Concept.objects.filter(
-                status=0, namespace__heading='Concepts'
-            ).count(),
-            79
-        )
-        self.assertEqual(
-            Concept.objects.filter(
-                status=0, namespace__heading='Groups'
-            ).count(),
-            2
-        )
+        # 45 new concepts were imported
+        num_concepts_after = Concept.objects.count()
+        self.assertEqual(num_concepts_after - num_concepts_before, 45)
+
         num_properties_before = Property.objects.count()
         # Import a separate spreadsheet only with translations
         import_obj = Import.objects.create(
@@ -72,6 +89,6 @@ class ConceptImportView(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         # The number of concepts is still the same
-        self.assertEqual(Concept.objects.filter(status=0).count(), 81)
+        self.assertEqual(Concept.objects.count(), num_concepts_after)
         # New properties were created with translations
         self.assertGreater(Property.objects.count(), num_properties_before)
