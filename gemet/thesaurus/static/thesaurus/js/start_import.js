5a5181ff05d37b4b1214629f88e74bff218311e0
Ariel Pontes <pontes.ariel@gmail.com>
2021-01-12 12:34:08 +0200

    Fix import URL construction in start_import.js

--- gemet/thesaurus/static/thesaurus/js/start_import.js
+++ gemet/thesaurus/static/thesaurus/js/start_import.js
@@ -1,7 +1,7 @@
 $ = django.jQuery;
 $(document).ready(function () {
     $('input.start-import').click(function () {
-        url = window.location.origin + "/import/" + $(this)[0].id + "/start/";
+        url = window.location.href.split("admin")[0] + "import/" + $(this)[0].id + "/start/";
         $.ajax({ url: url });
         location.reload();
     });
