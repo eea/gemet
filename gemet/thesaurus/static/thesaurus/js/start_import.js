dc86d0456caa942bf5021f08f78d90b277af3033
Ariel Pontes <pontes.ariel@gmail.com>
2021-01-25 10:50:47 +0200

    Fix import javascript

--- gemet/thesaurus/static/thesaurus/js/start_import.js
+++ gemet/thesaurus/static/thesaurus/js/start_import.js
@@ -1,8 +1,12 @@
 $ = django.jQuery;
 $(document).ready(function () {
-    $('input.start-import').click(function () {
+    $('input.start-import').click(function (event) {
         url = window.location.href.split("admin")[0] + "import/" + $(this)[0].id + "/start/";
-        $.ajax({ url: url });
-        location.reload();
+        $.ajax({
+            url: url, success: function (result) {
+                alert("Import started successfully.");
+                location.reload();
+            }
+        });
     });
 });
